3ef32ff7caeb4ee835d6441fe7692677d10c0878
EduardoD22 <edumoralez2205@gmail.com>
2024-09-12 20:44:04 -0600

    Morales version 1.2

--- src/pages/CreateJointRoom.jsx
+++ src/pages/CreateJointRoom.jsx
@@ -21,7 +21,7 @@ export default function CreateJoinRoom() {
             <div className="relative">
                     <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" size={18} />
                     <input id="name" placeholder="Código de Sala" className="pl-3 w-1/2 border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                    <button className="w-1/4 mt-6 ml-4 bg-blue-500 text-white py-2 rounded-lg">Unirse</button>
+                    <button className="w-1/4 mt-6 ml-6 bg-blue-500 text-white py-2 rounded-lg">Unirse</button>
             </div>
             <div className="relative">
             <button className="w-1/4 mt-6 bg-blue-500 text-white py-2 rounded-lg">Crear Sala</button>
