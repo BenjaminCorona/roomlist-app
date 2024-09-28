3edaf2f64ab1a7bed928be1801e1f0698d06134a
BenjaminCorona <benja13lara@gmail.com>
2024-09-27 22:40:24 -0600

    Nuevo diseño del tablero

--- src/pages/RoomList.jsx
+++ src/pages/RoomList.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CardTask from "../components/CardTask";
 import TaskItem from "../components/TaskItem";
 import UserItemList from "../components/UserItemList";
 import AddNewTask from "./AddNewTask";
-import '../index.css'
+import "../index.css";
 import {
   Bell,
   Inbox,
@@ -17,7 +17,7 @@ import {
   CircleCheckBig,
   Home,
   History,
-  HistoryIcon
+  HistoryIcon,
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
@@ -91,38 +91,50 @@ export default function RoomList() {
   };
 
   // Array de usuarios que se va a mostrar
-  const users = ['User1', 'User2', 'User3', 'User4', 'User5'];
-  const navigate = useNavigate(); 
-  
+  const users = ["User1", "User2", "User3", "User4", "User5"];
+  const navigate = useNavigate();
+
   const navigateHistory = () => {
-    navigate('/history');
-  }
+    navigate("/history");
+  };
 
   const navigateProfileSettings = () => {
-    navigate('/profile-settings');
-  }
+    navigate("/profile-settings");
+  };
+
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    // Generar un número aleatorio entre 1 y 14
+    const randomNumber = Math.floor(Math.random() * 14) + 1;
+    // Actualizar el estado con la ruta de la imagen aleatoria
+    setBackgroundImage(`/bg-roomlist/bg${randomNumber}.jpg`);
+  }, []); 
 
   return (
     //<div className="flex h-[600px] max-w-6xl mx-auto border rounded-lg overflow-hidden bg-gray-50">
-    <div className="flex h-screen w-screen mx-auto border rounded-lg overflow-hidden bg-gray-50">
+    <div
+      className="flex h-screen w-screen mx-auto border rounded-lg overflow-hidden bg-cover bg-center"
+      style={{ backgroundImage: `url(${backgroundImage})` }} // Fondo dinámico
+    >
       {/* Sidebar */}
       <div className="w-64 bg-white border-r p-4">
-        
         <button onClick={navigateProfileSettings} className="w-full">
-        <div className="flex items-center justify-between mb-6">
-          <div className="flex items-center">
-            <div className="h-8 w-8 mr-2 bg-gray-300 rounded-full flex items-center justify-center">
-              <span className="text-white">U</span>
+          <div className="flex items-center justify-between mb-6">
+            <div className="flex items-center">
+              <div className="h-8 w-8 mr-2 bg-gray-300 rounded-full flex items-center justify-center">
+                <span className="text-white">U</span>
+              </div>
+              <span className="font-semibold">User</span>
             </div>
-            <span className="font-semibold">User</span>
+            <ChevronDown size={20} />
           </div>
-          <ChevronDown size={20} />
-        </div>
         </button>
-        
+
         <button
-            onClick={toggleAddNewTaskModal}
-            className="w-full mb-4 bg-[#4b5563] hover:bg-gray-700 text-white py-2 px-4 rounded flex items-center">
+          onClick={toggleAddNewTaskModal}
+          className="w-full mb-4 bg-[#4b5563] hover:bg-gray-700 text-white py-2 px-4 rounded flex items-center"
+        >
           <Plus size={16} className="mr-2" /> Añadir tarea
         </button>
         {isAddNewTaskModalOpen && <AddNewTask toggle={toggleAddNewTaskModal} />}
@@ -130,7 +142,10 @@ export default function RoomList() {
           <button className="w-full justify-start py-2 px-4 rounded flex items-center bg-gray-200 hover:bg-gray-300">
             <Home size={16} className="mr-2" /> Inicio
           </button>
-          <button onClick={navigateHistory} className="w-full justify-start py-2 px-4 rounded flex items-center hover:bg-gray-300">
+          <button
+            onClick={navigateHistory}
+            className="w-full justify-start py-2 px-4 rounded flex items-center hover:bg-gray-300"
+          >
             <History size={16} className="mr-2" /> Historial
           </button>
           {/** 
@@ -162,7 +177,6 @@ export default function RoomList() {
         <div className="mt-6">
           <h3 className="font-semibold mb-2">Equipo</h3>
           {/**<ul className="space-y-1">
-             
             {teamProjects.map((project, index) => (
               <li key={index} className={`flex items-center ${project.color}`}>
                 <span className="mr-2">#</span>
@@ -172,16 +186,15 @@ export default function RoomList() {
               
           </ul>
           */}
-<div className="h-96 overflow-auto custom-scroll">
-  <UserItemList users={users} />
-</div>
-
+          <div className="h-96 overflow-auto custom-scroll">
+            <UserItemList users={users} />
+          </div>
         </div>
       </div>
 
       {/* Main Content */}
       <div className="flex-1 p-6 overflow-auto">
-        <h1 className="text-2xl text-gray-500 font-bold mb-4 ml-5">Inicio</h1>
+        <h1 className="text-2xl text-gray-100 font-bold mb-4 ml-5">Inicio</h1>
         {/**
         <div className="space-y-6">
           {["Mis Proyectos", "Equipo"].map((section) => (
@@ -234,25 +247,30 @@ export default function RoomList() {
           id="task-container"
           className="flex items-center w-full h-[80%] justify-evenly"
         >
-          <div className="bg-white shadow-lg flex flex-col items-center p-3 w-[50vw] h-full rounded-xl mr-3 ml-3 overflow-auto ">
-            <span
-                className="font-bold text-gray-600 text-xl flex items-center justify-center"><CircleCheckBig size={20} className="mr-2" /> To do</span>
-            <TaskItem title="Nueva tarea" user="user1"/>
+          <div className="bg-gray-300 bg-opacity-75 shadow-lg flex flex-col items-center p-3 w-[50vw] h-full rounded-xl mr-3 ml-3 overflow-auto">
+            <span className="font-bold text-gray-600 text-xl flex items-center justify-center">
+              <CircleCheckBig size={20} className="mr-2" /> To do
+            </span>
+            <TaskItem title="Nueva tarea" user="user1" />
             <TaskItem />
             <TaskItem />
             <TaskItem />
           </div>
 
-          <div className="bg-white shadow-lg flex flex-col items-center p-3 w-[50vw] h-full rounded-xl mr-3 ml-3 overflow-auto ">
-            <span className="font-bold text-gray-600 text-xl flex items-center justify-center"><CircleCheckBig size={20} className="mr-2"/> In progress</span>
+          <div className="bg-gray-300 bg-opacity-75 shadow-lg flex flex-col items-center p-3 w-[50vw] h-full rounded-xl mr-3 ml-3 overflow-auto">
+            <span className="font-bold text-gray-600 text-xl flex items-center justify-center">
+              <CircleCheckBig size={20} className="mr-2" /> In progress
+            </span>
             <TaskItem />
             <TaskItem />
             <TaskItem />
             <TaskItem />
           </div>
 
-          <div className="bg-white shadow-lg flex flex-col items-center p-3 w-[50vw] h-full rounded-xl mr-3 ml-3 overflow-auto ">
-            <span className="font-bold text-gray-600 text-xl flex items-center justify-center"><CircleCheckBig size={20} className="mr-2"/> Done</span>
+          <div className="bg-gray-300 bg-opacity-75 shadow-lg flex flex-col items-center p-3 w-[50vw] h-full rounded-xl mr-3 ml-3 overflow-auto">
+            <span className="font-bold text-gray-600 text-xl flex items-center justify-center">
+              <CircleCheckBig size={20} className="mr-2" /> Done
+            </span>
             <TaskItem />
             <TaskItem />
             <TaskItem />
