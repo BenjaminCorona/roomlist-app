4b8d41e54110f4617ddc24689c97e1c6150d9596
martinchavez022 <martinchavezest@gmail.com>
2024-11-18 10:17:27 -0600

    Fix date time in history

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import PocketBase from 'pocketbase';
+import { DateTime } from 'luxon'; // Asegúrate de haber instalado luxon
 
 const taskVariants = {
   offscreen: {
@@ -43,7 +44,6 @@ export default function History() {
     }
   }, [navigate]);
 
-  // Función para cargar la actividad desde PocketBase con los datos expandidos
   const loadActivityData = async () => {
     try {
       const storedRoomCode = localStorage.getItem("roomCode");
@@ -63,18 +63,24 @@ export default function History() {
       });
 
       // Mapeamos los datos obtenidos para ajustarlos a la estructura deseada
-      const formattedData = records.map((record) => ({
-        id: record.id,
-        Descripcion_Cambio: record.Descripcion_Cambio,
-        Fecha_Cambio: new Date(record.Fecha_Cambio).toLocaleString(),
-        expand: {
-          sala: {
-            id: record.expand?.ID_Sala?.id || "",
-            Nombre_Sala: record.expand?.ID_Sala?.Nombre_Sala || "",
-            Codigo_Sala: record.expand?.ID_Sala?.Codigo_Sala || "",
+      const formattedData = records.map((record) => {
+        const date = record.Fecha_Cambio.replace(' ', 'T');
+        const date2 = date.replace('Z', '');
+        const localDate = DateTime.fromISO(date2, { zone: 'America/Argentina/Buenos_Aires' });
+        const formattedDate = localDate.toFormat('dd/MM/yyyy HH:mm:ss');
+        return {
+          id: record.id,
+          Descripcion_Cambio: record.Descripcion_Cambio,
+          Fecha_Cambio: formattedDate,
+          expand: {
+            sala: {
+              id: record.expand?.ID_Sala?.id || "",
+              Nombre_Sala: record.expand?.ID_Sala?.Nombre_Sala || "",
+              Codigo_Sala: record.expand?.ID_Sala?.Codigo_Sala || "",
+            },
           },
-        },
-      }));
+        };
+      });
 
       setActivityData(formattedData);
     } catch (error) {
@@ -87,40 +93,40 @@ export default function History() {
   }, [filter]);
 
   return (
-    <div className="max-w-2xl mx-auto p-4 bg-white rounded-lg shadow">
-      <header className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold">Historial de Actividades</h1>
-        <DropdownMenu filter={filter} setFilter={setFilter} />
-      </header>
+      <div className="max-w-2xl mx-auto p-4 bg-white rounded-lg shadow">
+        <header className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-bold">Historial de Actividades</h1>
+          <DropdownMenu filter={filter} setFilter={setFilter} />
+        </header>
 
-      <motion.div className="max-h-80 overflow-y-auto overflow-x-hidden space-y-4">
-        <ul className="space-y-4">
-          {activityData.map((item) => (
-            <motion.li
-              initial="offscreen"
-              whileInView="onscreen"
-              viewport={{ once: true, amount: 0.4 }}
-              custom={item.id}
-              variants={taskVariants}
-              key={item.id}
-              className="flex flex-wrap items-start space-x-4 p-3 hover:bg-gray-50 rounded-md transition-colors"
-            >
-              <div className="flex-1 min-w-0">
-                <p className="text-sm text-gray-900 whitespace-normal">{item.Descripcion_Cambio}</p>
-                {item.expand.sala.Nombre_Sala && item.expand.sala.Codigo_Sala && (
-                  <p className="text-xs text-gray-500">
-                    Sala: {item.expand.sala.Nombre_Sala} ({item.expand.sala.Codigo_Sala})
-                  </p>
-                )}
-              </div>
-              <div className="flex items-center">
-                <span className="text-xs text-gray-400">{item.Fecha_Cambio}</span>
-              </div>
-            </motion.li>
-          ))}
-        </ul>
-      </motion.div>
-    </div>
+        <motion.div className="max-h-80 overflow-y-auto overflow-x-hidden space-y-4">
+          <ul className="space-y-4">
+            {activityData.map((item) => (
+                <motion.li
+                    initial="offscreen"
+                    whileInView="onscreen"
+                    viewport={{ once: true, amount: 0.4 }}
+                    custom={item.id}
+                    variants={taskVariants}
+                    key={item.id}
+                    className="flex flex-wrap items-start space-x-4 p-3 hover:bg-gray-50 rounded-md transition-colors"
+                >
+                  <div className="flex-1 min-w-0">
+                    <p className="text-sm text-gray-900 whitespace-normal">{item.Descripcion_Cambio}</p>
+                    {item.expand.sala.Nombre_Sala && item.expand.sala.Codigo_Sala && (
+                        <p className="text-xs text-gray-500">
+                          Sala: {item.expand.sala.Nombre_Sala} ({item.expand.sala.Codigo_Sala})
+                        </p>
+                    )}
+                  </div>
+                  <div className="flex items-center">
+                    <span className="text-xs text-gray-400">{item.Fecha_Cambio}</span>
+                  </div>
+                </motion.li>
+            ))}
+          </ul>
+        </motion.div>
+      </div>
   );
 }
 
@@ -135,44 +141,44 @@ function DropdownMenu({ filter, setFilter }) {
   };
 
   return (
-    <div className="relative">
-      <button
-        className="border px-4 py-2 rounded-md flex items-center"
-        onClick={toggleDropdown}
-      >
-        {filter}
-        <svg className="ml-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-        </svg>
-      </button>
-      {isOpen && (
-        <ul className="absolute bg-white border mt-2 rounded-md shadow-lg w-48">
-          <li
-            className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-            onClick={() => handleSelect("Toda la actividad")}
-          >
-            Toda la actividad
-          </li>
-          <li
-            className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-            onClick={() => handleSelect("Tareas añadidas")}
-          >
-            Tareas añadidas
-          </li>
-          <li
-            className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-            onClick={() => handleSelect("Tareas completadas")}
-          >
-            Tareas completadas
-          </li>
-          <li
-            className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-            onClick={() => handleSelect("Tareas modificadas")}
-          >
-            Tareas modificadas
-          </li>
-        </ul>
-      )}
-    </div>
+      <div className="relative">
+        <button
+            className="border px-4 py-2 rounded-md flex items-center"
+            onClick={toggleDropdown}
+        >
+          {filter}
+          <svg className="ml-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+          </svg>
+        </button>
+        {isOpen && (
+            <ul className="absolute bg-white border mt-2 rounded-md shadow-lg w-48">
+              <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                  onClick={() => handleSelect("Toda la actividad")}
+              >
+                Toda la actividad
+              </li>
+              <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                  onClick={() => handleSelect("Tareas añadidas")}
+              >
+                Tareas añadidas
+              </li>
+              <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                  onClick={() => handleSelect("Tareas completadas")}
+              >
+                Tareas completadas
+              </li>
+              <li
+                  className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                  onClick={() => handleSelect("Tareas modificadas")}
+              >
+                Tareas modificadas
+              </li>
+            </ul>
+        )}
+      </div>
   );
 }
--- src/tools/triggers_history.js
+++ src/tools/triggers_history.js
@@ -11,7 +11,7 @@ function getCurrentTime() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}Z`;
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
 }
 
 async function getRoomId(roomCode) {
